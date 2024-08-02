ba9a810f2d13ca39b22bc2c152e86f999d8a6f81
Jim Noble <jim.noble@milliman.com>
2024-08-02 13:31:49 -0400

    bug demonstration

---
+++ ServiceDiscoverySchemeDefect.Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine("Press ENTER to begin.");
+Console.ReadLine();
+
+var url = "https+http://myservice/weatherforecast";
+
+var configuration = new ConfigurationBuilder()
+    .AddInMemoryCollection(new Dictionary<string, string?>
+    {
+        ["Services:myservice:http:0"] = $"localhost:5083"
+    })
+    .Build();
+
+var serviceProvider = new ServiceCollection()
+    .AddSingleton<IConfiguration>(configuration)
+    .AddHttpClient()
+    .AddServiceDiscovery()
+    .ConfigureHttpClientDefaults(http =>
+    {
+        http.AddServiceDiscovery();
+    })
+    .BuildServiceProvider();
+
+var httpClient = serviceProvider
+    .GetRequiredService<IHttpClientFactory>()
+    .CreateClient();
+
+var response = await httpClient.GetAsync(url);
+
+Console.WriteLine($"Response: {response.StatusCode}");
+Console.ReadLine();
